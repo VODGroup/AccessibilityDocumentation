75aafede03704d25db2342f48a070057ab188654
Mikhail Rubanov <akaDuality@yandex.ru>
2023-12-07 21:41:55 -0300

    Add sample

---
+++ AccessibilityDocumentation/AccessibilityDocumentation/AccessibilityDocumentationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityDocumentationApp.swift
+//  AccessibilityDocumentation
+//
+//  Created by Mikhail Rubanov on 07.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityDocumentationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccessibilityDocumentation/AccessibilityDocumentation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AccessibilityDocumentation
+//
+//  Created by Mikhail Rubanov on 07.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
