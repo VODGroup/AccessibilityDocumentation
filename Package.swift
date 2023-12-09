1b3850c46c4eb97605ecb28b229c3b2533fa9ae7
Mikhail Rubanov <akaDuality@yandex.ru>
2023-12-09 07:55:50 -0300

    Add DocC dependencies

--- Package.swift
+++ Package.swift
@@ -10,6 +10,9 @@ let package = Package(
             name: "AccessibilityDocumentation",
             targets: ["AccessibilityDocumentation"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
+    ],
     targets: [
         .target(
             name: "AccessibilityDocumentation"),
